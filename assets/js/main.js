2e53a595e88b9ca5c4ca76d78d3351b5f5e34a7a
Thiago <thiagofang@gmail.com>
2022-10-17 21:53:25 -0300

    criando o javascript do menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
 
+const showMenu = () => {
+  navMenu.classList.add('show-menu');
+};
+if(navToggle) {
+  navToggle.addEventListener('click', showMenu);
+}
+
+const closeMenu = () => {
+  navMenu.classList.remove('show-menu');
+};
+if(navClose) {
+  navClose.addEventListener('click', closeMenu);
+};
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  navMenu.classList.remove('show-menu');
+};
+navLink.forEach(link => link.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
