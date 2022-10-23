add2e92c1400cffadc27d0c09302cfed0984a9b0
Thiago <thiagofang@gmail.com>
2022-10-23 19:11:30 -0300

    adicionando o efeito de escroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,19 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 1500,
+  delay: 400
+});
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`);
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800});
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800});
+sr.reveal(`.home__img`, {delay: 800});
+
+sr.reveal(`.category__card, .items__card`, {interval: 100});
+
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'});
+sr.reveal(`.party__img, .party__data, .footer__tree-1`, {origin: 'right'});
